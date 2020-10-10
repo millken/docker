a7d1304240b9c269bad50824d37e02c9b6f435a9
millken <millken@gmail.com>
2020-10-10 11:29:07 +0800

    up

---
+++ php/build.sh
@@ -0,0 +1,5 @@
+wget https://getcomposer.org/download/1.10.10/composer.phar
+wget https://github.com/laruence/yac/archive/master.zip
+wget http://pecl.php.net/get/msgpack-2.1.1.tgz
+wget https://github.com/swoole/swoole-src/archive/v4.4.20.tar.gz
+wget http://mirrors.sohu.com/php/php-7.4.9.tar.gz
